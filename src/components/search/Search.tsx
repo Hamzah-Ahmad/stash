a88a6c7c2335fff4d4dbb4a2611bcd2cb096dec3
Hamzah-Ahmad <ahmadhamzah500@gmail.com>
2026-04-26 10:45:09 -0500

    feat: basic typeahead funcotonality with keyboard nav

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,26 +1,40 @@
-import { useRef, useState, type ChangeEvent } from "react";
+import {
+  useEffect,
+  useRef,
+  useState,
+  type ChangeEvent,
+  type KeyboardEvent,
+} from "react";
 import useOnClickOutside from "../../hooks/useOnClickOutside";
 import type { SearchResult } from "./SearchWrapper";
+import { useStorageContext } from "../../context/StorageContext";
 
 type SearchProps = {
   onClose: () => void;
   search: (query: string) => any;
 };
 
-const ResultRow = ({ result }: { result: SearchResult }) => {
+const ResultRow = ({
+  result,
+  isFocused,
+}: {
+  result: SearchResult;
+  isFocused: boolean;
+}) => {
   const { endIndex, field, note, query, startIndex } = result;
   const substring = (note?.[field] as string)?.substring(
     startIndex,
     endIndex + 1,
   );
-
   const stringArr = substring.split(new RegExp(`(${query})`, "gi"));
-  console.log("LOGGER - stringArr: ", {stringArr, result})
   return (
-    <div className="p-4">
+    <div
+      className={`p-4 hover:bg-surface mb-2 ${isFocused ? "bg-surface" : ""}`}
+    >
       <div>{note?.title || "Untitled"}</div>
       <small>
-        ...{stringArr?.map((item) => (
+        ...
+        {stringArr?.map((item) => (
           <>
             {item?.toLowerCase() === query?.toLowerCase() ? (
               <span className="bg-red-300">{item}</span>
@@ -28,20 +42,55 @@ const ResultRow = ({ result }: { result: SearchResult }) => {
               <>{item}</>
             )}
           </>
-        ))}...
+        ))}
+        ...
       </small>
     </div>
   );
 };
 const Search = ({ onClose, search }: SearchProps) => {
   const ref = useRef<HTMLDivElement>(null);
+  const { handleSelectNote } = useStorageContext();
 
   const [results, setResults] = useState<SearchResult[] | null>(null);
+  const [focusedNoteIndx, setFocusedNoteIndx] = useState<number>(-1);
 
   function handleSearch(e: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
     const searchResults = search(e.target?.value);
     if (Array.isArray(searchResults)) {
       setResults(searchResults);
+    }
+  }
+
+  useEffect(() => {
+    setFocusedNoteIndx(-1);
+  }, [results]);
+
+  console.log("LOGGER - focusedNoteIndx: ", focusedNoteIndx);
+
+  function onKeyDown(e: KeyboardEvent<HTMLInputElement>) {
+    console.log("LOGGER - here", e.key);
+
+    if (e.key === "ArrowUp") {
+      setFocusedNoteIndx((prev) => {
+        if (prev === null || prev === -1) return prev;
+        return prev - 1;
+      });
+    }
+
+    if (e.key === "ArrowDown") {
+      setFocusedNoteIndx((prev) => {
+        if (!results?.length || prev === results?.length - 1) return prev;
+        return prev + 1;
+      });
+    }
+
+    if (e.key === "Enter") {
+      const foundResult = results?.[focusedNoteIndx];
+      if (foundResult && foundResult?.note) {
+        handleSelectNote(foundResult.note?.id);
+        onClose();
+      }
     }
   }
 
@@ -54,11 +103,15 @@ const Search = ({ onClose, search }: SearchProps) => {
   useOnClickOutside(ref, handleOnClickOutside);
   return (
     <div className="search-modal" ref={ref}>
-      <input autoFocus onChange={handleSearch} />
+      <input autoFocus onChange={handleSearch} onKeyDown={onKeyDown} />
       {results?.length ? (
-        <div className="h-fit w-full bg-surface-2">
-          {results.map((result: SearchResult) => (
-            <ResultRow result={result} key={result?.note?.id} />
+        <div className="h-fit max-h-96 w-full bg-surface-2 overflow-y-auto">
+          {results.map((result: SearchResult, index) => (
+            <ResultRow
+              result={result}
+              key={result?.note?.id}
+              isFocused={index === focusedNoteIndx}
+            />
           ))}
         </div>
       ) : null}
